406fe739faa913a549fa7295b636d1c8a15e29f9
System administrator <fayezmohammed23@gmail.com>
2025-07-06 02:50:30 +0000

    feat: bash it added'

--- home/fayez.nix
+++ home/fayez.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
-{
+let
+  bashItSrc = builtins.fetchGit {
+    url = "https://github.com/Bash-it/bash-it.git";
+    rev = "4c5ac697f593169ab09a63e0f78f85a20d01c47a";  # Replace with the latest commit if needed
+  };
+in {
   home.username = "fayez";
   home.homeDirectory = "/home/fayez";
 
@@ -10,34 +15,41 @@
       if [[ -f "${pkgs.blesh}/share/blesh/ble.sh" ]]; then
         source "${pkgs.blesh}/share/blesh/ble.sh"
       fi
+
+      export BASH_IT="${config.home.homeDirectory}/.bash_it"
+      export BASH_IT_THEME="powerline-multiline"
+      source "$BASH_IT/bash_it.sh"
     '';
+
+  };
+
+  home.file.".bash_it" = {
+    source = bashItSrc;
+    recursive = true;
   };
 
   home.packages = with pkgs; [
     blesh
   ];
 
+  systemd.user.services.set-wallpaper = {
+    Unit = {
+      Description = "Set GNOME wallpaper using gsettings";
+      After = [ "graphical-session.target" ];
+    };
 
+    Service = {
+      ExecStart = ''
+        ${pkgs.gsettings-desktop-schemas}/bin/gsettings set org.gnome.desktop.background picture-uri "file:///home/fayez/Wallpapers/zero-two.jpg"
+        ${pkgs.gsettings-desktop-schemas}/bin/gsettings set org.gnome.desktop.background picture-options "centered"
+      '';
+    };
 
-systemd.user.services.set-wallpaper = {
-  Unit = {
-    Description = "Set GNOME wallpaper using gsettings";
-    After = [ "graphical-session.target" ];
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
   };
-
-  Service = {
-    ExecStart = ''
-      ${pkgs.gsettings-desktop-schemas}/bin/gsettings set org.gnome.desktop.background picture-uri "file:///home/fayez/Pictures/zero-two.jpg"
-      ${pkgs.gsettings-desktop-schemas}/bin/gsettings set org.gnome.desktop.background picture-options "centered"
-    '';
-  };
-
-  Install = {
-    WantedBy = [ "default.target" ];
-  };
-};
-
-
 
   home.stateVersion = "25.05";
 }
+
